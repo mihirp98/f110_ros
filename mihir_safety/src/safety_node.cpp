def316415ee8bbddd1659b52866bbcb18e0a89d5
Mihir <mihir.parmar1998@gmail.com>
2020-08-06 16:10:30 -0400

    update safety node

--- mihir_safety/src/safety_node.cpp
+++ mihir_safety/src/safety_node.cpp
@@ -15,7 +15,7 @@ class Safety {
 private:
     ros::NodeHandle n;
     double speed;
-    // TODO: create ROS subscribers and publishers
+
     ros::Publisher brake_bool_pub;
     ros::Publisher acker_brake_pub;
     ros::Subscriber scan_sub;
@@ -24,7 +24,6 @@ private:
 public:
     double num;
     double den;
-//    double ttc;
     double ttc_thres;
     std_msgs::Bool bool_msg;
     ackermann_msgs::AckermannDriveStamped brake_msg;
@@ -34,32 +33,12 @@ public:
     double min_ttc;
     double angle;
     std::vector<double> ttc{vector<double>(1080,0)};
-//    std::array<double, 1080> ttc;
     Safety() {
         n = ros::NodeHandle();
         speed = 0.0;
         ttc_thres = 0.35;
-//        ttc = 0.0;
         start_angle = -3.14159274101;
         increment = 0.00582315586507;
-        /*
-        One publisher should publish to the /brake topic with an
-        ackermann_msgs/AckermannDriveStamped brake message.
-
-        One publisher should publish to the /brake_bool topic with a
-        std_msgs/Bool message.
-
-        You should also subscribe to the /scan topic to get the
-        sensor_msgs/LaserScan messages and the /odom topic to get
-        the nav_msgs/Odometry messages
-
-        The subscribers should use the provided odom_callback and 
-        scan_callback as callback methods
-
-        NOTE that the x component of the linear velocity in odom is the speed
-        */
-
-        // TODO: create ROS subscribers and publishers
         
         odom_sub = n.subscribe("/odom",1,&Safety::odom_callback,this);
 	    scan_sub = n.subscribe("/scan",1,&Safety::scan_callback,this);
@@ -68,7 +47,6 @@ public:
         
     }
     void odom_callback(const nav_msgs::Odometry::ConstPtr &odom_msg) {
-        // TODO: update current speed
         speed = odom_msg->twist.twist.linear.x;
         
     }
@@ -86,8 +64,7 @@ public:
     }
 
     void scan_callback(const sensor_msgs::LaserScan::ConstPtr &scan_msg) {
-        // TODO: calculate TTC
-//        std::array<double, 1080> ttc;
+
         if(speed <= 0){
             ttc_thres = 0.38;
         }else{
@@ -109,14 +86,9 @@ public:
         }else {
             bool_msg.data = false;
             brake_bool_pub.publish(bool_msg);
-        }
-//        min_ttc = *min_element(ttc.begin(), ttc.end());
-        
-        
-        
+        }       
     
         ROS_INFO("Safety Node Running");
-        // TODO: publish drive/brake message
     }
 
 };
